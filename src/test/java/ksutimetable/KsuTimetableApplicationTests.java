c77ec48869d700f85b57c8db5c870ac818c9eb3d
AndreiDevyaterikov <devyaterikov.andrei@gmail.com>
2022-08-21 22:47:43 +0300

    init project

---
+++ src/test/java/ksutimetable/KsuTimetableApplicationTests.java
@@ -0,0 +1,13 @@
+package ksutimetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsuTimetableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
