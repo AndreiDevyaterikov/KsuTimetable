70ef8b08589861d9f522b9a9a9c7ee53ad4166b8
AndreiDevyaterikov <devyaterikov.andrei@gmail.com>
2022-08-22 00:22:23 +0300

    rename SwaggerConfig.setAPI

--- src/main/java/ksutimetable/configs/SwaggerConfig.java
+++ src/main/java/ksutimetable/configs/SwaggerConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI springShopOpenAPI() {
+    public OpenAPI setAPI() {
         return new OpenAPI()
                 .info(new Info().title("Ksu timetable API"));
     }
