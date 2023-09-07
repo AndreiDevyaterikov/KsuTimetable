c3a6211b48df9d545349aa2b043240a7173661e2
AndreiDevyaterikov <devyaterikov.andrei@gmail.com>
2023-09-07 14:50:26 +0300

    Удалил WebClientConfig

--- src/main/java/ksutimetable/configs/RestTemplateConfig.java
+++ src/main/java/ksutimetable/configs/RestTemplateConfig.java
@@ -4,15 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import org.springframework.web.util.UriTemplateHandler;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 @Configuration
 public class RestTemplateConfig {
@@ -22,24 +19,16 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-
         RestTemplate restTemplate = new RestTemplate();
         UriTemplateHandler uriTemplateHandler = new DefaultUriBuilderFactory(defaultUri);
         restTemplate.setUriTemplateHandler(uriTemplateHandler);
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.TEXT_HTML));
-        restTemplate.getMessageConverters().add(converter);
+        restTemplate.getMessageConverters().add(getJackson2Converter());
         return restTemplate;
     }
 
-    private List<HttpMessageConverter<?>> getMessageConverters() {
-
-        List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
-
+    private MappingJackson2HttpMessageConverter getJackson2Converter() {
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.ALL));
-        messageConverters.add(converter);
-
-        return messageConverters;
+        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.TEXT_HTML));
+        return converter;
     }
 }
